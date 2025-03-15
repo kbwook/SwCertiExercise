0e7dc76f16fdf002cad9b632b13ad624220b1b1f
kbwook <kbwook@gmail.com>
2025-03-15 11:47:18 +0900

    TourPackageRecommend base code and sample input add

---
+++ TourPackageRecommend/solutioncpp.cpp
@@ -0,0 +1,24 @@
+void init(int N, int M)
+{
+
+}
+
+void befriend(int uid1, int uid2)
+{
+
+}
+
+void add(int pid, int area, int price)
+{
+
+}
+
+void reserve(int uid, int pid)
+{
+
+}
+
+int recommend(int uid)
+{
+	return 0;
+}
